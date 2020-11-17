fee11563d00710419aef6494080e21e9d140beff
HeatherKmtb <heather.kaymtb@gmail.com>
2020-11-17 10:49:25 +0000

    update footprints number

--- final.py
+++ final.py
@@ -1088,7 +1088,7 @@ def grid_only(folderin, fileout, folderout, naming=2):
 
         footprints = len(final['i_h100'])
         
-        if footprints < 10:
+        if footprints < 50:
             continue
         
         #NEXT STEP. Bin remaining data in order to get mean and IQR of each bin
