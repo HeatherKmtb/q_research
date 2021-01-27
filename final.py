94bdbd9ff37d5532cc612d747bc1df3692ec5bdd
HeatherKmtb <heather.kaymtb@gmail.com>
2021-01-27 09:27:35 +0000

    adding obatin_removed_polygons to final

--- final.py
+++ final.py
@@ -603,7 +603,7 @@ def biomes(folderin, fileout, folderout, naming='gla14_eco_'):
         plt.close
 
 
-def grid(folderin, fileout, folderout, naming=3, eco_loc=2):
+def grid(folderin, fileout, folderout, naming=4, eco_loc=2):
     """
     Function to compute q and provide results (csv) and figures (pdf)
     
@@ -676,6 +676,7 @@ def grid(folderin, fileout, folderout, naming=3, eco_loc=2):
         footprints = len(final['i_h100'])
         
         if footprints < 100:
+            print(name +'_' + eco)
             continue
         
         #NEXT STEP. Bin remaining data in order to get mean and IQR of each bin
@@ -1231,4 +1232,79 @@ def grid_only(folderin, fileout, folderout, naming=2):
         ax.annotate('No of footprints = ' + str(footprints),xy=(0.975,0.05), xycoords='axes fraction', fontsize=12, horizontalalignment='right', verticalalignment='bottom')
         plt.savefig(folderout + 'fig{}.pdf'.format(name))
         plt.close
+        
+def obtain_removed_polygons(folderin, naming=4, eco_loc=2):
+    """
+    Function to compute q and provide results (csv) and figures (pdf)
+    
+    Parameters
+    ----------
+    
+    folderin: string
+            Filepath for folder with files ready for analysis
+                
+    naming: int
+          Section of filename to obtain ID (here grid number). Obtained
+          by splitting filename by '_' and indexing
+          Default = 3
+
+    eco_loc: int
+          Section of filename to obtain ecoregion (if applicable). 
+          Obtained as with naming
+          Default = 2
+          
+    fileout: string
+           Filepath for results file ending '.csv'
+           
+    folderout: string
+             Filepath for folder to save the figures            
+    """
+
+    #using 'file' to title plot  
+    fileList = glob.glob(folderin + '*.shp')
+
+    #create df for results
+    #resultsa = pd.DataFrame(columns = ['eco', 'ID', 'qout', 'r_sq', 'deg_free', 'rmse','r_sq_mean', 'adj_r2'])
+    #resultsb = pd.DataFrame(columns = ['eco', 'ID', 'qout', 'r_sq', 'deg_free', 'rmse'])
+
+    for file in fileList:
+        df = gpd.read_file(file)
+        if df.empty:
+            continue 
+        hd, tl = path.split(file)
+        shp_lyr_name = path.splitext(tl)[0]
+        name_comp = shp_lyr_name.split('_')
+        name = name_comp[naming] 
+        eco = name_comp[eco_loc]
+        print(name)
+        print(eco)
+        #remove data with H_100 >= 0 prior to logging
+        test2 = df[df['i_h100']>=0] 
+        
+        #means x is just the h100 data - needs logging to normalise (not skewed) 
+        x = test2['i_h100']
+        
+        #create new column in df with log of H_100 
+        y = np.log(x)
+        test2a = test2.assign(log_i_h100 = y)
+        
+        if test2a.empty:
+            continue
+
+        #get quantiles
+        a = np.quantile(test2a['log_i_h100'],0.95)
+        b = np.quantile(test2a['log_i_h100'],0.05)
+
+        #remove data outside of 5% quantiles
+        test3 = test2a[test2a.log_i_h100 >b]
+        final = test3[test3.log_i_h100 <a]
+
+        if final.empty:
+            continue
+        del a, b, x, y, test2, test2a, test3
+
+        footprints = len(final['i_h100'])
+        
+        if footprints < 100:
+            print(name +'_' + eco)        
                 
